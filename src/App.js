87191a70789f31066f0ee9700ae7cf9df4703769
Bartlomiej Figatowski <bartek_19_83@hotmail.com>
2018-07-11 23:58:57 -0400

    practise

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from "react";
+import "./App.css";
+import Navigation from "./components/layout/Navbar";
+
+const App = () => {
+  return (
+    <div>
+      <Navigation />
+    </div>
+  );
+};
+
+export default App;
---
+++ src/components/layout/Navbar.js
@@ -0,0 +1,14 @@
+import React from "react";
+import { Navbar } from "react-bootstrap";
+
+const Navigation = (
+  <Navbar>
+    <Navbar.Header>
+      <Navbar.Brand>
+        <a href="/">React-Bootstrap</a>
+      </Navbar.Brand>
+    </Navbar.Header>
+  </Navbar>
+);
+
+export default Navigation;
